377b667db10caf223b098d604ec6e806b25a4e72
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-02-16 20:09:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
-import { Provider } from "./context/context";
 import "./App.css";
+import Form from "./components/Form";
+import axios from "axios";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    currentWeather: {},
+    weatherList: []
+  };
+
+  getWeather = (weather, city) => {
+    const API_KEY = "74a0d65d1017ee174d9493121f8fca0b";
+    const country = "US";
+    this.setState({
+      currentWeather: weather
+    });
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=imperial`
+      )
+      .then(res =>
+        this.setState({
+          weatherList: [
+            res.data.list[5],
+            res.data.list[13],
+            res.data.list[21],
+            res.data.list[28],
+            res.data.list[32]
+          ]
+        })
+      );
+  };
 
   render() {
     return (
-      <Provider>
-        <div className="App">app</div>
-      </Provider>
+      <div className="App">
+        <Form getWeather={this.getWeather} />
+      </div>
     );
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
+import { InputGroup, InputGroupAddon, Button, Input } from "reactstrap";
+import axios from "axios";
+import moment from "moment";
 
-const Form = props => (
-  <form onSubmit={props.getWeather}>
-    <input type="text" name="city" placeholder="City..." />
-    <button>Get Weather</button>
-  </form>
-);
+export default class Form extends Component {
+  state = {
+    text: ""
+  };
 
-export default Form;
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    const API_KEY = "74a0d65d1017ee174d9493121f8fca0b";
+    const country = "US";
+    const cityName = this.state.text;
+    event.preventDefault();
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${country}&appid=${API_KEY}&units=imperial`
+      )
+      .then(res => {
+        this.props.getWeather(
+          {
+            city: res.data.name,
+            country: res.data.sys.country,
+            day: moment().format("dddd"),
+            date: moment().format("MMMM Do YYYY"),
+            img: `http://openweathermap.org/img/w/${
+              res.data.weather[0].icon
+            }.png`,
+            temp: Math.round(res.data.main.temp),
+            description: res.data.weather[0].description,
+            humidity: res.data.main.humidity,
+            pressure: res.data.main.pressure,
+            wind: Math.round(res.data.wind.speed)
+          },
+          cityName
+        );
+        this.setState({
+          text: ""
+        });
+      });
+  };
+
+  render() {
+    return (
+      <form className="search_bar" onSubmit={this.handleSubmit}>
+        <InputGroup>
+          <Input
+            name="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+            placeholder="Enter City..."
+          />
+          <InputGroupAddon addonType="append">
+            <Button color="info" onSubmit={this.handleSubmit}>
+              <i className="fas fa-search" />
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </form>
+    );
+  }
+}
