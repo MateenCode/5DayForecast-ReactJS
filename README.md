ba3f0e556f4b8717e83a58fcf9ccb494c06d511a
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-02-19 20:22:00 -0800

    read me added build file created

---
+++ README.md
@@ -0,0 +1,10 @@
+# FiveDay Forecast
+
+five day forecast application created using react, it display the current weather and dynamicaly updates from weathermap API.
+
+# ScreenShot
+
+<img width="857" alt="screen shot 2019-02-19 at 2 35 38 pm" src="https://user-images.githubusercontent.com/28902787/53066085-22294180-3483-11e9-9440-73e82fc22ac9.png">
+<img width="816" alt="screen shot 2019-02-19 at 2 35 55 pm" src="https://user-images.githubusercontent.com/28902787/53066088-26555f00-3483-11e9-97ec-5f815aab4e57.png">
+
+**[View Live](https://mateenshowcase.herokuapp.com/)**
---
+++ build/precache-manifest.116ccad68dbf666b966fc1701c5f8a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25da6b08a606f92ad916",
+    "url": "/static/js/main.25da6b08.chunk.js"
+  },
+  {
+    "revision": "e40a1b08b7a82434909a",
+    "url": "/static/js/1.e40a1b08.chunk.js"
+  },
+  {
+    "revision": "25da6b08a606f92ad916",
+    "url": "/static/css/main.f57d7d0b.chunk.css"
+  },
+  {
+    "revision": "e40a1b08b7a82434909a",
+    "url": "/static/css/1.eefea4fc.chunk.css"
+  },
+  {
+    "revision": "c4f00a1660a56a83fc8d01e113b97b50",
+    "url": "/index.html"
+  }
+];
