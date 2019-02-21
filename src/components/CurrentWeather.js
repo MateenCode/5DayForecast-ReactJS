8e84c5b26451f81229e6a6daa66bc4cd97b31d29
Mateen Kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-02-21 15:28:54 -0800

    destructoring added

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,36 +1,48 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-
 import "../css/Weather.css";
 
 export default function CurrentWeather(props) {
+  const {
+    city,
+    country,
+    day,
+    date,
+    description,
+    img,
+    temp,
+    temp_max,
+    temp_min,
+    humidity,
+    wind
+  } = props.weather;
   return (
     <Container className="main">
       <div className="header_key">
         <span className="h2">
-          {props.weather.city}, {props.weather.country}
+          {city}, {country}
         </span>
         <p className="text-secondary">
-          {props.weather.day}, {props.weather.date}
+          {day}, {date}
           <br />
-          {props.weather.description}
+          {description}
         </p>
       </div>
       <Row>
         <Col>
-          <img className="icon" src={props.weather.img} alt="" />
-          <span className="temp_key">{props.weather.temp}°</span>
+          <img className="icon" src={img} alt="" />
+          <span className="temp_key">{temp}°</span>
         </Col>
         <Col>
           <p className="text-secondary">
             <span className="text-white h6">Temperature: </span>
-            {props.weather.temp_max}° ~ {props.weather.temp_min}°
+            {temp_max}° ~ {temp_min}°
             <br />
             <span className="text-white h6">Humidity: </span>
-            {props.weather.humidity}%
+            {humidity}%
             <br />
             <span className="text-white h6">Wind: </span>
-            {props.weather.wind} mph
+            {wind} mph
           </p>
         </Col>
       </Row>
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,24 +1,30 @@
 import React from "react";
 import Moment from "react-moment";
+import PropTypes from "prop-types";
 import "../css/Weather.css";
 
 export default function WeatherList(props) {
+  const { changeWeather, weatherItem } = props;
+  const { dt_txt, weather, main } = weatherItem;
   return (
-    <div className="weather" onClick={props.changeWeather}>
+    <div className="weather" onClick={changeWeather}>
       <div className="weather_list">
-        <Moment format="dddd">{props.weatherItem.dt_txt}</Moment>
+        <Moment format="dddd">{dt_txt}</Moment>
         <br />
         <img
           className="icon_list"
-          src={`http://openweathermap.org/img/w/${
-            props.weatherItem.weather[0].icon
-          }.png`}
+          src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
           alt=""
         />
         <br />
-        {Math.round(props.weatherItem.main.temp)}
+        {Math.round(main.temp)}
         <span>°F</span>
       </div>
     </div>
   );
 }
+
+WeatherList.propTypes = {
+  changeWeather: PropTypes.func.isRequired,
+  weatherItem: PropTypes.object.isRequired
+};
