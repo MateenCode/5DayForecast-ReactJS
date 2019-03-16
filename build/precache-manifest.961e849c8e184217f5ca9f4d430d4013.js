b3ba98b0f0aaf8213aa28ff6b4c5636a248f1f00
Mateen Kazia <mateencode@gmail.com>
2019-03-16 13:08:49 -0700

    make it better

---
+++ build/precache-manifest.961e849c8e184217f5ca9f4d430d4013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc4c0968cda101c91fbd",
+    "url": "/static/js/main.dc4c0968.chunk.js"
+  },
+  {
+    "revision": "8bf6c6a17c6b6e07a759",
+    "url": "/static/js/1.8bf6c6a1.chunk.js"
+  },
+  {
+    "revision": "dc4c0968cda101c91fbd",
+    "url": "/static/css/main.a75f38b5.chunk.css"
+  },
+  {
+    "revision": "8bf6c6a17c6b6e07a759",
+    "url": "/static/css/1.8688abec.chunk.css"
+  },
+  {
+    "revision": "17a7a60e50f2a9189f945ee3d4088f4a",
+    "url": "/index.html"
+  }
+];
